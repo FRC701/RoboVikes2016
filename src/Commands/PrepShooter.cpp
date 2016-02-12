58149841c649004513e2dff22dc6e76a602a460a
Ryan L <RdogsH@gmail.com>
2016-02-12 12:09:08 -0800

    Created PrepShooter

---
+++ src/Commands/PrepShooter.cpp
@@ -0,0 +1,25 @@
+#include "PrepShooter.h"
+#include "SetShooter.h"
+#include "ShooterControl.h"
+
+PrepShooter::PrepShooter()
+{
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+	AddSequential(new SetShooter('f'));
+	AddSequential(new ShooterControl(1.0));
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+}
