bda41b4446e5128535b447b226b6865398e6793d
KillerAsuna <writingbloc99@gmail.com>
2016-02-21 17:15:42 -0800

    Self Test not finished

---
+++ src/Commands/SelfTestCommand.h
@@ -0,0 +1,24 @@
+#ifndef SelfTestCommand_H
+#define SelfTestCommand_H
+
+#include "Commands/Subsystem.h"
+#include "../Robot.h"
+
+class SelfTestCommand: public Command
+{
+public:
+	SelfTestCommand();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+private:
+	bool isChassisGood;
+	bool isIntakeGood;
+	bool isArmsGood;
+	bool isShooterGood;
+	bool isShooterLift;
+};
+
+#endif
