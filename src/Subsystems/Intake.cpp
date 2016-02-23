57c4271017b7984dffb964eea232f9682a9c0cbe
Ryan L <RdogsH@gmail.com>
2016-02-23 14:32:43 -0800

    changed the down limit

--- src/Subsystems/Intake.cpp
+++ src/Subsystems/Intake.cpp
@@ -22,13 +22,13 @@
 
 //static const int kIntakeMotor3AbsoluteOffset = 1131; //practice bot
 //static const int kIntakeMotor4AbsoluteOffset = 2342;
-static const int kIntakeMotor3AbsoluteOffset = 1947;
-static const int kIntakeMotor4AbsoluteOffset = 1594;
+static const int kIntakeMotor3AbsoluteOffset = 1974;
+static const int kIntakeMotor4AbsoluteOffset = 1743;
 
 static const double kIntakeMotor3ForwardPositionLimit =  0.160;
-static const double kIntakeMotor3ReversePositionLimit = -0.03;
+static const double kIntakeMotor3ReversePositionLimit = -1.0;//-0.03;
 static const double kIntakeMotor4ForwardPositionLimit =  0.03;
-static const double kIntakeMotor4ReversePositionLimit = -0.160;
+static const double kIntakeMotor4ReversePositionLimit = -1.0;//-0.160;
 static const double kIntakeMotorProfile1_P = 10.0;
 static const double kIntakeMotorProfile1_F = 5.0;
 
